95ab611d3bf315cc73e143d228ff19539d4d5d74
ivo2018 <ivo_argentina_07@hotmail.com>
2023-07-31 14:15:31 -0300

    a

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,100 +17,100 @@ import "../components/About.css"
 const About = () => {  
   const [opacitystate,setOpacityState] = useState(false);
 
+
 //  const [description,setDescription] = useState("I'm Ivo, full stack developer from Buenos Aires This is my website, a place where I share my projects   ,  let my imagination run wild and let people know things about me.");
     function changeColor(){
+      
+        var work,about,skills,contact;
+        work=document.getElementById('work');
+        about=document.getElementById('about');
+        skills=document.getElementById('skills');
+        contact=document.getElementById('contact');
         var element =document.querySelector('.homePage__folders-work');
         if(element){
         element.addEventListener("click",
         function (event) {
-            document.getElementById('work').style.outline='dotted 1.2px rgb(255, 255, 255)';
-            document.getElementById('work').style.background='rgb(2, 2, 80)';
-            document.getElementById('about').style.outline='none';
-            document.getElementById('about').style.background='none';
-            document.getElementById('skills').style.outline='none';
-            document.getElementById('skills').style.background='none';
-            document.getElementById('about').style.outline='none';
-           document.getElementById('about').style.background='none';
-            document.getElementById('contact').style.outline='none';
-            document.getElementById('contact').style.background='none';
+            work.style.outline='dotted 1.2px rgb(255, 255, 255)';
+            work.style.background='rgb(2, 2, 80)';
+           skills.style.outline='none';
+            skills.style.background='none';
+           about.style.outline='none';
+          about.style.background='none';
+           contact.style.outline='none';
+          contact.style.background='none';
             
         })};
      var element2 =document.querySelector('.homePage__folders-skills');
      if(element2){
     element2.addEventListener("click",
     function (event) {
-        document.getElementById('skills').style.outline='dotted 1.2px rgb(255, 255, 255)';
-        document.getElementById('skills').style.background='rgb(2, 2, 80)';
-        document.getElementById('work').style.outline='none';
-        document.getElementById('work').style.background='none';
-        document.getElementById('about').style.outline='none';
-        document.getElementById('about').style.background='none';
-
-        document.getElementById('about').style.outline='none';
-        document.getElementById('about').style.background='none';
-        document.getElementById('contact').style.outline='none';
-        document.getElementById('contact').style.background='none';
+     skills.style.outline='dotted 1.2px rgb(255, 255, 255)';
+        skills.style.background='rgb(2, 2, 80)';
+       work.style.outline='none';
+       work.style.background='none';
+       about.style.outline='none';
+      about.style.background='none';
+       contact.style.outline='none';
+      contact.style.background='none';
     })};
     var element3 =document.querySelector('.homePage__folders-message');
     if(element3){
     element3.addEventListener("click",
     function (event) {
-    document.getElementById('contact').style.outline='dotted 1.2px rgb(255, 255, 255)';
-    document.getElementById('contact').style.background='rgb(2, 2, 80)';
-    document.getElementById('work').style.outline='none';
-    document.getElementById('work').style.background='none';
-    document.getElementById('about').style.outline='none';
-    document.getElementById('about').style.background='none';
-    document.getElementById('skills').style.outline='none';
-    document.getElementById('skills').style.background='none';
-    document.getElementById('about').style.outline='none';
-    document.getElementById('about').style.background='none';
-
+   contact.style.outline='dotted 1.2px rgb(255, 255, 255)';
+    contact.style.background='rgb(2, 2, 80)';
+    skills.style.outline='none';
+    skills.style.background='none';
+   about.style.outline='none';
+  about.style.background='none';
+   work.style.outline='none';
+ work.style.background='none';
     })}
     var element4 =document.querySelector('.homePage__folders-about');
     if(element4){
     element4.addEventListener("click",
     function (event) {
-    document.getElementById('about').style.outline='dotted 1.2px rgb(255, 255, 255)';
-    document.getElementById('about').style.background='rgb(2, 2, 80)';
-    document.getElementById('work').style.outline='none';
-    document.getElementById('work').style.background='none';
-    document.getElementById('skills').style.outline='none';
-    document.getElementById('skills').style.background='none';
-    document.getElementById('contact').style.outline='none';
-    document.getElementById('contact').style.background='none';
+    about.style.outline='dotted 1.2px rgb(255, 255, 255)';
+    about.style.background='rgb(2, 2, 80)';
+    skills.style.outline='none';
+    skills.style.background='none';
+   work.style.outline='none';
+  work.style.background='none';
+   contact.style.outline='none';
+  contact.style.background='none';
 })}
 
 
  }
  useEffect(() => {
     function titleEffect(){
+        var titlePageName=    document.getElementById('content-title');
     var element =document.querySelector('.homePage');
     if(element){
     element.addEventListener("mousemove",
     function (event) {
         if(event.movementX >0){
-      document.getElementById('content-title').style.boxShadow="15px 6px 5px -2px rgba(77, 33, 33, 0.568)";
+            titlePageName.style.boxShadow="15px 6px 5px -2px rgba(77, 33, 33, 0.568)";
  
-      document.getElementById('content-title').style.transition="0.8s all";
+            titlePageName.style.transition="0.8s all";
         }   else
         if(event.movementX<0)    {
-            document.getElementById('content-title').style.boxShadow="-15px -6px 5px -2px rgba(77, 33, 33, 0.568)";
+            titlePageName.style.boxShadow="-15px -6px 5px -2px rgba(77, 33, 33, 0.568)";
    
-            document.getElementById('content-title').style.transition="0.8s all";
+            titlePageName.style.transition="0.8s all";
         }  else  
         if(event.movementY<0 )    {
-            document.getElementById('content-title').style.boxShadow="0px -6px 2px 0.5px rgba(77, 33, 33, 0.568)";
+            titlePageName.style.boxShadow="0px -6px 2px 0.5px rgba(77, 33, 33, 0.568)";
        
-            document.getElementById('content-title').style.transition="0.8s all";
+            titlePageName.style.transition="0.8s all";
         }  else
         if(event.movementY>0)    {
-            document.getElementById('content-title').style.boxShadow="0px 6px 2px 0.5px rgba(77, 33, 33, 0.568)";
+            titlePageName.style.boxShadow="0px 6px 2px 0.5px rgba(77, 33, 33, 0.568)";
           
-            document.getElementById('content-title').style.transition="0.8s all";
+            titlePageName.style.transition="0.8s all";
         }  
         else{
-            document.getElementById('content-title').style.boxShadow="0px 6px 2px 0.5px rgba(77, 33, 33, 0.568)";
+            titlePageName.style.boxShadow="0px 6px 2px 0.5px rgba(77, 33, 33, 0.568)";
         }
 
       
@@ -120,43 +120,45 @@ const About = () => {
 
   }
   function opacity(){
+    var key1 , key2;
+    key1= document.getElementById("key1")
+    key2= document.getElementById("key2")
     var element = document.querySelector(".homePage__content-key1");
     if(element){
         element.addEventListener("click",
         function (event){
             if(opacitystate===false){
-            document.getElementById("key2").style.opacity="0.99";  
-            document.getElementById("key1").style.opacity="0.5";     
+            key2.style.opacity="0.99";  
+           key1.style.opacity="0.5";     
             setOpacityState(true);
         ///    setDescription("Here are my personal interests, for professional purposes you can be interested in my work or my skills. Thanks So Mutch")
           
   
             }
         })}
-    }
-    function opacity2(){
         var element2 = document.querySelector(".homePage__content-key2");
         if(element2){
             element2.addEventListener("click",
             function (event){
                if(opacitystate===true)
-                document.getElementById("key2").style.opacity="0.5";
-                document.getElementById("key1").style.opacity="0.99";   
+                key2.style.opacity="0.5";
+              key1.style.opacity="0.99";   
            //     setDescription("I'm Ivo, full stack developer from Buenos Aires This is my website, a place where I share my projects   ,  let my imagination run wild and let people know things about me.")
                 setOpacityState(false);
             })}
-      
-    };
-    function opacity3(){
+    }
+    
+    function closeWindow(){
+        var windowAbout=  document.getElementById("windowAbout");
         var element3 = document.querySelector(".homePage__about-title img");
         if(element3){
             element3.addEventListener("click",
             function (event){
       
-                document.getElementById("father").style.opacity="0";
-                document.getElementById("father").style.width="20%"; 
+                windowAbout.style.opacity="0";
+                windowAbout.style.width="20%"; 
 
-                document.getElementById("father").style.transition="0.2s all"
+                windowAbout.style.transition="0.2s all"
                 
                
             })}
@@ -165,8 +167,8 @@ const About = () => {
                 element4.addEventListener("click",
                 function (event){
           
-                    document.getElementById("father").style.opacity="100%";
-                    document.getElementById("father").style.width="90%"
+                    windowAbout.style.opacity="100%";
+                    windowAbout.style.width="60%"
 
 
                    
@@ -175,7 +177,7 @@ const About = () => {
     };
 
 
-  titleEffect();opacity();opacity2();opacity3();});
+  titleEffect();opacity();closeWindow();});
 
     return (
         <div class="homePage">
@@ -215,7 +217,7 @@ const About = () => {
             </div>
         </div>
      
-        <div id="father" class="homePage__about" >
+        <div id="windowAbout" class="homePage__about" >
       
             <div class="homePage__about-title">
      
