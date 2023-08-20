3e80ff572e2d61324b6815b06181932f262a18ee
ivo2018 <ivo_argentina_07@hotmail.com>
2023-08-20 02:05:54 -0300

    a

--- src/lists/listsCarouselDividerWeb.js
+++ src/lists/listsCarouselDividerWeb.js
@@ -1,13 +1,20 @@
 import foto from "../imgs/2-2.jpg"
-import foto2 from "../imgs/2-0.jpg"
+//import foto2 from "../imgs/2-0.jpg"
+import Home from "../imgs/Web/DarteArte/Home_Shot.png"
+import Home2 from "../imgs/Web/DarteArte/Home2_Shot.png"
+//import Carro from "../imgs/Web/DarteArte/Carro_Shot.png"
+import ProductDetail from "../imgs/Web/DarteArte/ProductDetail_shot.png"
+import ProductDetailBuy from "../imgs/Web/DarteArte/ProductDetailBuy_shot2.png"
 
 var list = [
 
     {
         id:"1 ",
         name: "DarteArte",
-        photo: foto,
-        photo2:foto2,
+        photo: Home,
+        photo2:Home2,
+        photo3: ProductDetail,
+        photo4:ProductDetailBuy,
         description:" University Pompeu Fabra website for Masters degree information and purchase, blog, and educational related content. The website consists of the following parts: a CMS section for content creation by the university using Drupal with customized modules. This data is integrated with the university's API in a backend that is then collected by Nextjs to serve as the frontend"
         
   ,detalle:"detalles......"  },
