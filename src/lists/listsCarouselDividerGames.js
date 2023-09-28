c03f170e688330fdf9605f43f00b90ef6fbe3008
ivo2018 <ivo_argentina_07@hotmail.com>
2023-09-28 01:18:50 -0300

    a

--- src/lists/listsCarouselDividerGames.js
+++ src/lists/listsCarouselDividerGames.js
@@ -6,6 +6,11 @@ import Bandera from "../imgs/Videojuegos/Zemaria/Bandera.png"
 import ConsigoLanzaCohetes from "../imgs/Videojuegos/Zemaria/ConsigoLanzaCohetes.png"
 import Smoke from "../imgs/Videojuegos/Zemaria/SmokeAturdido.png"
 import Explosion from "../imgs/Videojuegos/Zemaria/Explosion.png"
+import MainScene from "../imgs/Videojuegos/Sekeletons/MainScene.png";
+import MainScene2 from "../imgs/Videojuegos/Sekeletons/MainScene2.png";
+import MainScene3 from "../imgs/Videojuegos/Sekeletons/MainScene3.png";
+import MainScene5 from "../imgs/Videojuegos/Sekeletons/MainScene5.png";
+
 
 var list=[
     {
@@ -19,8 +24,11 @@ var list=[
  ,detalles:"detalles......."   },
     {
         id:"2",
-        name: "Zemaria",
-        photo:photo,
+        name: "Skeletons",
+        photo:MainScene,
+        photo2:MainScene2,
+        photo3:MainScene3,
+        photo4:MainScene5,
         description:"Infojobs Assistant is an extension for Google Chrome that allows the user to get an in-depth analysis of their resume in relation to the job offer they want to apply for. The purpose of the extension is to facilitate the selection process for the candidate so that they feel more confident and increase their chances of getting the job"
  ,detalles:"detalles......."   },
     {
